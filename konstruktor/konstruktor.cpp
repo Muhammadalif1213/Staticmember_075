be0ab733c4f428a8c6c84201bd734dd5bde20099
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-05-17 18:43:18 +0700

    mendefinisikan method cetak

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -31,3 +31,7 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 	nim = iNim;
 	nama = iNama;
 }
+void mahasiswa::cetak() {
+	cout << endl << "nim =" << nim << endl;
+	cout << "nama = " << nama << endl;
+}
